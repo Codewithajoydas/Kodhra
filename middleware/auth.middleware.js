dfdba1565ac3e1221a599f75545615d81ea88d95
codewithajoydas@gmail.com <codewithajoydas@gmail.com>
2025-08-17 12:47:27 +0530

    Last commit for the day

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,18 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
 const authMiddleware = (req, res, next) => {
-      const token = req.cookies.token;  
-    if (!token) {
-        return res.status(401).json({ message: 'Access denied. No token provided.' });
-    }
-    try {
-        const secret = process.env.SECRET;
-        const decoded = jwt.verify(token, secret);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        return res.status(403).json({ message: 'Invalid or expired token.' });
-    }
+  const token = req.cookies.token;
+  if (!token) {
+    return res.redirect("/login");
+  }
+  try {
+    const secret = process.env.SECRET;
+    const decoded = jwt.verify(token, secret);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "Invalid or expired token." });
+  }
 };
 module.exports = authMiddleware;
